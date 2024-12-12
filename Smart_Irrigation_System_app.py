a258d97a5b670fc328865e2b648558c6906ae07b
Igor Augusto Campos Barreto <59749870+igbarretoIB@users.noreply.github.com>
2024-12-12 00:09:32 -0300

    app v1.2

--- Smart_Irrigation_System_app.py
+++ Smart_Irrigation_System_app.py
@@ -5,6 +5,13 @@ import pandas as pd
 import pydeck as pdk
 import folium
 from streamlit_folium import st_folium
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st    
 
 # Configurar layout da página como wide
 st.set_page_config(layout="wide")
@@ -23,7 +30,7 @@ st.write(' ')
 st.write(' ')
 st.write(' ')
 st.write(' ')
-tab1, tab2, tab3 = st.tabs(["Clima", "Estufa", "Dashboard"])
+tab1, tab2, tab3, tab4 = st.tabs(["Clima", "Estufa", "Adubação" ,"Dashboard"])
 with tab1:
     # Primeira divisão horizontal
     with st.container():
@@ -484,6 +491,20 @@ with tab1:
         unsafe_allow_html=True
         )
         st.write(' ')
+        st.write(' ')
+        st.write(' ')
+        st.markdown(
+        """
+        <h5 style='text-align: left;'>
+            Quarta-feira, 11/12/2024
+        </h5>
+        """, 
+        unsafe_allow_html=True
+        )
+        st.write(' ')
+        st.write(' ')
+        st.write(' ')
+        st.write(' ')
 
         # Primeira divisão horizontal
         with st.container():
@@ -585,6 +606,7 @@ with tab2:
     """, 
     unsafe_allow_html=True
     )
+    
     st.write(' ')
     st.write(' ')
     st.write(' ')
@@ -608,40 +630,9 @@ with tab2:
     st.write(' ')
     st.write(' ')
     
-        # Coluna da direita (com 2 divisões horizontais)
+    # Coluna da direita (com 2 divisões horizontais)
 
-    st.markdown(
-    """
-    <h2 style='text-align: center;'>
-        Nível de Nutrientes
-    </h2>
-    """, 
-    unsafe_allow_html=True
-    ) 
-    st.write(' ')
-    st.write(' ')
-    st.write(' ')
-    # Primeira divisão horizontal
-    with st.container():
-        # Divisão vertical dentro da última divisão horizontal
-        subcol21, subcol22, subcol23, subcol24, subcol25, subcol26, subcol27= st.columns(7)
-        with subcol23:
-            st.metric(label="N", value="255", delta="10")  
-            
-        with subcol24:
-            st.metric(label="P", value="255", delta="10")
-            
-        with subcol25:
-            st.metric(label="K", value="255", delta="10")
-    
-    st.write(' ')
-    st.write(' ')
-    st.write(' ')
-    st.write(' ')
-    st.write(' ')
-    st.write(' ')
-            
-    
+       
     # Segunda divisão horizontal
     with st.container():
         # Divisão vertical dentro da última divisão horizontal
@@ -661,43 +652,104 @@ with tab2:
             st.write(' ')
             st.write(' ')
             st.write(' ')
-            st.write(' ')
-            st.write(' ')
-            st.write(' ')
+            
             st.markdown(
                     """
-                    <h4 style='text-align: center;'>
-                        Pump On/ Pump Off
-                    </h4>
+                    <h1 style='text-align: center;'>
+                       50 %
+                    </h1>
                     """, 
                     unsafe_allow_html=True
                 )
-    
+            
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')
+            
+            st.markdown(
+                    """
+                    <h3 style='text-align: center;'>
+                     Pump status:
+                    </h3>
+                    """, 
+                    unsafe_allow_html=True
+                )
+            
+            
+            # st.markdown(
+            #         """
+            #         <h3 style='text-align: center;>
+            #            Pump status:
+            #         </h3>
+            #         """, 
+            #         unsafe_allow_html=True
+            #     )
+            
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')  
+            st.markdown(
+                    """
+                    <h1 style='text-align: center;color:#0A7E31'>
+                        On
+                    </h1>
+                    """, 
+                    unsafe_allow_html=True
+                )
+            
         with subcol32:
             st.markdown(
                     """
-                    <h4 style='text-align: center;'>
+                    <h3 style='text-align: center;'>
                         Probabilidade de Ligar a Fan
-                    </h4>
+                    </h3>
                     """, 
                     unsafe_allow_html=True
                 )
-            
-            st.write(' ')
-            st.write(' ')
-            st.write(' ')
             st.write(' ')
             st.write(' ')
             st.write(' ')
             
             st.markdown(
                     """
-                    <h4 style='text-align: center;'>
-                        Fan On/ Fan Off
-                    </h4>
+                    <h1 style='text-align: center;'>
+                       70 %
+                    </h1>
                     """, 
                     unsafe_allow_html=True
                 )
+            
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')               
+            
+            st.markdown(
+                    """
+                    <h3 style='text-align: center;'>
+                    Fan status:
+                    </h3>
+                    """, 
+                    unsafe_allow_html=True
+                )
+            
+            st.write(' ')
+            st.write(' ')
+            st.write(' ')  
+            st.markdown(
+                    """
+                    <h1 style='text-align: center;color:#6a040f'>
+                        Off
+                    </h1>
+                    """, 
+                    unsafe_allow_html=True
+                )
+            
     st.write(' ')
     st.write(' ')
     st.write(' ')
@@ -706,6 +758,124 @@ with tab2:
     st.write(' ')
  
 with tab3:
+
+    st.markdown(
+    """
+    <h2 style='text-align: center;'>
+        Nível de Nutrientes
+    </h2>
+    """, 
+    unsafe_allow_html=True
+    ) 
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    # Primeira divisão horizontal
+    with st.container():
+        # Divisão vertical dentro da última divisão horizontal
+        subcol21, subcol22, subcol23, subcol24, subcol25, subcol26, subcol27= st.columns(7)
+        with subcol22:
+            st.metric(label="N", value="255", delta="10")  
+            
+        with subcol24:
+            st.metric(label="P", value="255", delta="10")
+            
+        with subcol26:
+            st.metric(label="K", value="255", delta="10")
+        
+        subcol221, subcol222, subcol223= st.columns(3)
+        with subcol222:
+            # Função para criar o gráfico de radar
+            # def plot_radar(data):
+            #     # Preparar dados
+            #     labels = data['Variáveis'].tolist()
+            #     valores = data['Valores'].tolist()
+            #     max_valores = [255] * len(labels)
+
+            #     # Fechar os polígonos
+            #     valores += valores[:1]
+            #     max_valores += max_valores[:1]
+
+            #     # Ângulos para cada variável
+            #     angulos = np.linspace(0, 2 * np.pi, len(labels), endpoint=False).tolist()
+            #     angulos += angulos[:1]
+
+            #     # Criar o gráfico
+            #     fig, ax = plt.subplots(figsize=(5, 5), subplot_kw={'projection': 'polar'})
+            #     fig.patch.set_facecolor("#0D1117")
+            #     # Traçar o triângulo maior (máximos)
+            #     ax.plot(angulos, max_valores, color="gray", linewidth=1.5, linestyle="--", label="Máximo")
+
+            #     # Traçar o triângulo menor (valores reais)
+            #     ax.fill(angulos, valores, color="#0A7E31", alpha=0.25, label="Valores Reais")
+            #     ax.plot(angulos, valores, color="#0A7E31", linewidth=2)
+
+            #     # Estilo do gráfico
+            #     ax.set_facecolor("#0D1117")  # Fundo branco
+            #     #ax.set_facecolor((1, 1, 1, 0))  # Fundo transparente (sem preenchimento)
+            #     #ax.grid(color="gray", linestyle="--", linewidth=0.5)
+            #     ax.spines['polar'].set_visible(False)  # Remover borda externa
+            #     ax.set_yticks([])  # Remover rótulos dos círculos
+
+            #     # Adicionar rótulos das variáveis
+            #     ax.set_xticks(angulos[:-1])
+            #     ax.set_xticklabels(labels, fontsize=12, color="white")
+
+
+            #     return fig
+
+            def plot_radar(data): 
+                labels = data['Variáveis'].tolist()
+                valores = data['Valores'].tolist() 
+                max_valores = [255] * len(labels) 
+                
+                # Fechar os polígonos 
+                valores += valores[:1] 
+                max_valores += max_valores[:1] 
+                labels += labels[:1] 
+                fig = go.Figure() 
+                
+                # Adicionar o triângulo maior (máximos) 
+                fig.add_trace(go.Scatterpolar( r=max_valores, 
+                                               theta=labels, 
+                                               fill='toself', 
+                                               name='Máximo', 
+                                               line=dict(color='gray', dash='dash') 
+                                               )
+                              ) 
+                # Adicionar o triângulo menor (valores reais) 
+                fig.add_trace(go.Scatterpolar( r=valores, 
+                                               theta=labels, 
+                                               fill='toself', 
+                                               name='Valores Reais', 
+                                               line=dict(color='#0A7E31') 
+                                               )
+                              ) 
+                fig.update_layout(polar=dict( bgcolor='#0D1117', 
+                                              radialaxis=dict(visible=False), 
+                                              angularaxis=dict(tickfont=dict(color='white')) 
+                                              ), 
+                                  showlegend=False, # Remover a legenda 
+                                  paper_bgcolor='#0D1117', 
+                                  font=dict(color='white'), 
+                                  width=500, # Aumentar a largura da área de plotagem 
+                                  height=500 # Aumentar a altura da área de plotagem 
+                                  ) 
+                return fig
+            
+            # Dados
+            data = {
+                'Variáveis': ['N 255', 'P 255', 'K 255'],
+                'Valores': [120, 180, 150],
+            }
+
+            df = pd.DataFrame(data)
+            # Exibir o gráfico
+            fig = plot_radar(df)
+            #st.pyplot(fig)
+            # Mostrar o gráfico no Streamlit
+            st.plotly_chart(fig, key='radar nutrientes')    
+with tab4:
     st.write(' ')
     st.write(' ')
     st.write(' ')
@@ -713,60 +883,121 @@ with tab3:
     st.write(' ')
     st.write(' ')   
     # Gerar gráficos de exemplo
-    def generate_plot(title):
-        fig, ax = plt.subplots()
-        x = np.linspace(0, 10, 100)
-        y = np.sin(x)
-        ax.plot(x, y)
-        ax.set_title(title)
+    # def generate_plot(data, x, y):
+    #     fig, ax = plt.subplots()
+    #     fig.patch.set_facecolor("#0D1117")
+        
+    #     sns.lineplot(data=data, x=x, y=y, ax=ax, color='green')
+        
+    #     ax.set_facecolor("#0D1117")
+    #     # ax.set_title(title, color='white')
+    #     ax.tick_params(colors='white')
+    #     ax.xaxis.label.set_color('white')
+    #     ax.yaxis.label.set_color('white')
+        
+    #     return fig
+    
+    def generate_plot(data, x, y):
+        fig = px.line(data, x=x, y=y, title='Gráfico de Linhas', line_shape='linear') #
+        fig.update_traces(line=dict(color='green'), mode='lines+markers')
+        #fig.update_traces(line=dict(color='#0A7E31', mode='lines+markers'))
+        fig.update_layout( plot_bgcolor='#0D1117', paper_bgcolor='#0D1117', font_color='white' ) 
         return fig
     
-    st.write(' ')
-    st.write(' ')
-    st.write(' ')
-    st.markdown(
-    """
-    <h2 style='text-align: center;'>
-        Temperatura
-    </h2>
-    """, 
-    unsafe_allow_html=True
-    )
+    data = pd.DataFrame({ 'x': range(10), 'y': [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] })
     
+    
+    with st.container():
+        # Divisão vertical dentro da última divisão horizontal
+        subcol311, subcol312, subcol313, subcol314, subcol315= st.columns(5)
+        with subcol312:
+            st.markdown(
+                    """
+                    <h3 style='text-align: center;'>
+                    Pump On count
+                    </h3>
+                    """, 
+                    unsafe_allow_html=True
+                )
+            
+            subcol321, subcol322, subcol323= st.columns(3)
+            with subcol322:
+                st.metric(label="  ", value="35")
+                
+        with subcol314:
+            st.markdown(
+                    """
+                    <h3 style='text-align: center;'>
+                    Fan On count
+                    </h3>
+                    """, 
+                    unsafe_allow_html=True
+                )
+            
+            subcol331, subcol332, subcol333= st.columns(3)
+            with subcol332:
+                st.metric(label="  ", value="35")
     # Primeira divisão horizontal
-    with st.container():
-        st.pyplot(generate_plot("Gráfico 1"))
-        
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
     st.write(' ')
     st.write(' ')
     st.write(' ')
     
-    st.markdown(
-    """
-    <h2 style='text-align: center;'>
-        Umidade
-    </h2>
-    """, 
-    unsafe_allow_html=True
-    )  
-    # Segunda divisão horizontal
-    with st.container():
-        st.pyplot(generate_plot("Gráfico 2"))
+    with st.container():        
+        subcol411, subcol412 = st.columns(2)
+        with subcol411:
+            st.markdown(
+                """
+                <h2 style='text-align: center;'>
+                    Temperatura
+                </h2>
+                """, 
+                unsafe_allow_html=True
+            )
+            #st.pyplot(generate_plot(data, 'x', 'y'))
+            # Gerar o gráfico 
+            fig = generate_plot(data, 'x', 'y')
+            st.plotly_chart(fig, key='temperatura')
+            
+            with subcol412:
+                st.markdown(
+                    """
+                    <h2 style='text-align: center;'>
+                        Umidade
+                    </h2>
+                    """, 
+                    unsafe_allow_html=True
+                )
+                #st.pyplot(generate_plot(data, 'x', 'y'))
+                # Gerar o gráfico 
+                fig = generate_plot(data, 'x', 'y')
+                st.plotly_chart(fig, key='umidade')
+                  
+                
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
     
-    st.write(' ')
-    st.write(' ')
-    st.write(' ')
-    st.markdown(
-    """
-    <h2 style='text-align: center;'>
-        Nível de água
-    </h2>
-    """, 
-    unsafe_allow_html=True
-    )  
-    # Terceira divisão horizontal
     with st.container():
-        st.pyplot(generate_plot("Gráfico 3"))
-    
+        subcol421, subcol422 = st.columns(2)
+        with subcol421:
+            st.markdown(
+                    """
+                    <h2 style='text-align: center;'>
+                        Nível de água
+                    </h2>
+                    """, 
+                    unsafe_allow_html=True
+                )  
+            #st.pyplot(generate_plot(data, 'x', 'y'))
+            # Gerar o gráfico 
+            fig = generate_plot(data, 'x', 'y')
+            st.plotly_chart(fig, key='nivel de agua')
+            
 with st.sidebar:
     st.title('Menu')
